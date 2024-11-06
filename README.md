ef6cefee14dea0788e7262ad39f5c29001ba24f0
dgenutis <dainiusgenutis@yahoo.com>
2024-11-06 12:46:34 +0200

    done

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# React + Vite
+https://dgenutis-passgenerator.netlify.app/
+
+# React + Vite, TailwindCSS
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
@@ -7,6 +7,36 @@ function App() {
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState('')
+
+
+  const passwordRef = useRef(null)
+
+  const generatePassword = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (numberAllowed) str += "0123456789"
+    if (charAllowed) str += "!@#$%^&*()-_=+[{]};:',<.>/?"
+    
+    for(let i = 1; i < length; i++) {
+    const char = Math.floor(Math.random() * str.length + 1)
+    pass += str.charAt(char)
+    }
+
+    setPassword(pass)
+  }, [length, numberAllowed, charAllowed])
+
+
+const copyPasswordToClipboard = () => {
+  window.navigator.clipboard.writeText(password)
+  passwordRef.current?.select()
+}
+
+
+useEffect(() => {
+  generatePassword()
+}, [length, numberAllowed, charAllowed])
+
 
   return (
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
@@ -18,8 +48,11 @@ function App() {
           className="outline-none w-full py-1 px-3"
           placeholder="password"
           readOnly
+          ref={passwordRef}
         />
-        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+        onClick={copyPasswordToClipboard}
+        >
           copy
         </button>
       </div>
